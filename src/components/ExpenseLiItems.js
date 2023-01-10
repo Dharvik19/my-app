36e958b43ffac0f92fff01d265a295f62f3eabac
Dharvik19 <dharvik.badga22>
2023-01-10 13:15:31 +0530

    commit to react

---
+++ src/components/ExpenseLiItems.js
@@ -0,0 +1,12 @@
+function ExpenseLiItems(){
+    return (
+    <>
+    <li>Expense Items</li>
+    <li>Food Rs 10</li>
+    <li>Petrol Rs 100</li>
+    <li>Movies Rs 200</li>
+    
+    </>      
+);
+}
+export default ExpenseLiItems;
