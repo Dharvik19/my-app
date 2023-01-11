488a283beb621a25e9a02fc797bbba878be13f87
Dharvik19 <dharvik.badga22@gmail.com>
2023-01-11 13:24:51 +0530

    Added Css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,23 @@
+import './ExpenseItem.css';
 function ExpenseItem(){
-    return <h2>Expense Item!!</h2>
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle='Car insurance';
+    const expenseAmount = 256;
+    const LocationOfExpenditure =  window.location.hostname;
+    return (
+        <>
+        <h2>Expense Item!!</h2>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div>{LocationOfExpenditure}</div>
+            <div className="expense-item__description"> 
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
+            </div>
+        </div>
+        </>
+        
+    );
 
 }
 export default ExpenseItem;
