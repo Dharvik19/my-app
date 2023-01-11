f5d6d7cc990b4630b21b438428bb4821b2ad14d2
Dharvik19 <dharvik.badga22@gmail.com>
2023-01-11 18:06:07 +0530

    introduction to props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
 import ExpenseItem from './components/ExpenseItem';
-import ExpenseLiItems from './components/ExpenseLiItems';
+// import ExpenseLiItems from './components/ExpenseLiItems';
 function App() {
+  let expenses = [
+    { 
+      id:1,
+      title:'toilet paper',
+      amount:'94',
+      date: new Date(2021, 11,0)
+    },
+    {
+      id:2,
+      title:'car service',
+      amount:'300',
+      date: new Date(2022, 0,1)
+    },
+    {
+      id:3,
+      title:'snacks ',
+      amount:'10',
+      date: new Date(2022, 0,1)
+    },
+    {
+      id:4,
+      title:'movies',
+      amount:'500',
+      date: new Date(2022, 0,1)
+    }
+  ]
+  // return (
+  //   <div className="App">
+  //       <h2>lets get started </h2> 
+  //       <ExpenseItem 
+  //       title={expenses[0].title}
+  //       amount={expenses[0].amount}
+  //       date = {expenses[0].date}
+  //       ></ExpenseItem>
+  //       <ExpenseItem 
+  //       title={expenses[1].title}
+  //       amount={expenses[1].amount}
+  //       date = {expenses[1].date}
+  //       ></ExpenseItem>
+  //       <ExpenseItem 
+  //       title={expenses[2].title}
+  //       amount={expenses[2].amount}
+  //       date = {expenses[2].date}
+  //       ></ExpenseItem>
+  //       <ExpenseItem 
+  //       title={expenses[3].title}
+  //       amount={expenses[3].amount}
+  //       date = {expenses[3].date}
+        
+  //       ></ExpenseItem>
+        
+       
+  //       <ExpenseLiItems></ExpenseLiItems>
+  //   </div>
+  // );
   return (
-    <div className="App">
-        <h2>lets get started </h2> 
-        <ExpenseItem></ExpenseItem>
-        <ExpenseLiItems></ExpenseLiItems>
-    </div>
-  );
+         <div >
+            <h2>Expense Tracker</h2>
+            { 
+              expenses.map(expense =>{
+                return (
+                 
+                  <div key={expense.id} className="App">
+                     
+                    <ExpenseItem 
+                      title={expense.title}
+                      amount={expense.amount}
+                      date = {expense.date}
+
+                      ></ExpenseItem>
+                  </div>
+                );
+              })
+            }
+         </div> 
+    );
 }
 export default App;
