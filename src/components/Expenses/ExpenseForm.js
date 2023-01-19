2a67da9ccde88041f71b6e27159ac03a6306d2f2
Dharvik19 <dharvik.badga22@gmail.com>
2023-01-19 17:53:16 +0530

    made some changes in the form and added css

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,24 +1,30 @@
 import React from "react";
+import "./ExpenseForm.css";
+import './NewExpense.css'
 function ExpenseForm() {
-    const titleChangeHandler = (e)=>{
-        console.log(e.target.value);
-    }
+  const titleChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <>
       <form>
-        <label>
-          title:
-          <input type="text" name="name" onChange={titleChangeHandler}/>
-        </label>
-        <label>
-          Amount:
-          <input type="text" name="amount" />
-        </label>
-        <label>
-          Date:
-          <input type="date" name="date" />
-        </label>
-        <input type="submit" value="Submit" />
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>title:</label>
+            <input type="text" name="name" onChange={titleChangeHandler} />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount:</label>
+            <input type="number" name="amount" />
+          </div>
+          <div className="new-expense__control">
+            <label>Date:</label>
+            <input type="date" name="date" />
+          </div>
+          <div className='new-expense__actions'>
+            <button  type="submit">Add Expense</button>
+          </div>
+        </div>
       </form>
     </>
   );
