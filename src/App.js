377c0d4178e01c986da2245dcbb780591ae27f0e
Dharvik19 <dharvik.badga22@gmail.com>
2023-01-21 18:23:22 +0530

    dynamic data

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-
 // import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import React,{useState} from 'react';
+// import ExpenseItem from './components/ExpenseItem';
+// import Card from './components/UI/Card';
+import Expenses from './components/Expenses/Expenses';
+import ExpenseForm from './components/Expenses/ExpenseForm';
+import NewExpense from './components/Expenses/NewExpense';
+// import ExpenseDate from './components/Expenses/ExpenseDate';
 // import ExpenseLiItems from './components/ExpenseLiItems';
+
+const expenses1 = [
+  { 
+    id:1,
+    title:'toilet paper',
+    amount:'94',
+    date: new Date(2021, 11,0)
+  },
+  {
+    id:2,
+    title:'car service',
+    amount:'300',
+    date: new Date(2022, 0,1)
+  },
+  {
+    id:3,
+    title:'snacks ',
+    amount:'10',
+    date: new Date(2022, 0,1)
+  },
+  {
+    id:4,
+    title:'movies',
+    amount:'500',
+    date: new Date(2022, 0,1)
+  }
+]
 function App() {
-  let expenses = [
-    { 
-      id:1,
-      title:'toilet paper',
-      amount:'94',
-      date: new Date(2021, 11,0)
-    },
-    {
-      id:2,
-      title:'car service',
-      amount:'300',
-      date: new Date(2022, 0,1)
-    },
-    {
-      id:3,
-      title:'snacks ',
-      amount:'10',
-      date: new Date(2022, 0,1)
-    },
-    {
-      id:4,
-      title:'movies',
-      amount:'500',
-      date: new Date(2022, 0,1)
-    }
-  ]
+  
   // return (
-  //   <div className="App">
+  //   <Card className="App">
   //       <h2>lets get started </h2> 
   //       <ExpenseItem 
   //       title={expenses[0].title}
@@ -55,29 +62,44 @@ function App() {
   //       ></ExpenseItem>
         
        
-  //       <ExpenseLiItems></ExpenseLiItems>
-  //   </div>
+       
+  //   </Card>
   // );
-  return (
-         <div >
-            <h2>Expense Tracker</h2>
-            { 
-              expenses.map(expense =>{
-                return (
-                 
-                  <div key={expense.id} className="App">
-                     
-                    <ExpenseItem 
-                      title={expense.title}
-                      amount={expense.amount}
-                      date = {expense.date}
-
-                      ></ExpenseItem>
-                  </div>
-                );
-              })
-            }
-         </div> 
-    );
+  // 
+  // return(
+  //   <>
+  //     <div> 
+  //       <h2>Lets get started </h2>
+  //       <Expenses items={expenses}/>
+  //     </div>
+  //   </>
+  // )
+useState(expenses1);
+  const [expenses, setExpenses] = useState(expenses1);
+const addExpenseHandler = expense =>{
+  console.log("In app.js");
+  console.log(expenses);
+  setExpenses(prevExpense =>{
+    return [expense, ...prevExpense];
+  });
+};
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, 'lets get started'),
+  //   // React.createElement(ExpenseDate,{date:date}),
+  //   React.createElement(NewExpense),
+  //   React.createElement(Expenses,{items:expenses}),
+  //   React.createElement(NewExpense,{onAddExpense:addExpenseHandler})
+  // );
+ return(
+  <>
+  <div>
+  <NewExpense onAddExpense={addExpenseHandler}/>;
+  <Expenses items={expenses}/>
+  </div>
+  </>
+ )
 }
 export default App;
+
