d27629747cafaa1a579398bf6a6cdf5413a0c502
Dharvik19 <dharvik.badga22@gmail.com>
2023-01-21 13:03:08 +0530

    added the form inputs as objects

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -5,22 +5,51 @@ function ExpenseForm() {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
+// const [userInput, setUserInput]=useState({
+//     enteredTitle: '',
+//     enteredAmount:'',
+//     enteredDate:''
+// });
   const titleChangeHandler = (e) => {
     setEnteredTitle(e.target.value);
-    console.log(enteredTitle);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredTitle:e.target.value
+    // });
+    // setUserInput((prevState)=>{
+    //     return { ...prevState, enteredTitle: e.target.value};
+    // });
+    // console.log(enteredTitle);
   };
 
   const amountChangeHandler = (e) =>{
     setEnteredAmount(e.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredAmount:e.target.value
+    // })
     console.log(enteredAmount);
   }
   const dateChangeHandler = (e) =>{
     setEnteredDate(e.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredDate:e.target.value
+    // })
     console.log(enteredDate);
+  }
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    const expenseData={
+      title: enteredTitle,
+      amount: enteredAmount,
+      date:new Date(enteredDate)
+    };
+    console.log(expenseData);
   }
   return (
     <>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>title:</label>
